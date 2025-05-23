63c7c49f323595c704bddb39dc6d0029891b1315
AffanRifai <affanrifai268@gmail.com>
2025-05-23 14:45:32 +0700

    affan commit

--- halamanbooking.php
+++ halamanbooking.php
@@ -44,6 +44,7 @@ if (isset($_GET['logout'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking Lapangan</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="stylesheet" href="halamanbooking.css">
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
